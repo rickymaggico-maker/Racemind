f291b4e05f35c83c2d4c70fd31aad6905a2cb9c5
rickymaggico-maker <ricky.maggico@gmail.com>
2026-09-09 10:32:27 +0200

    Separate WPF app from bootstrap entry point

--- src/RaceMind/App.xaml.cs
+++ src/RaceMind/App.xaml.cs
@@ -1,16 +1,7 @@
 using System.Windows;
-using Velopack;
 
 namespace RaceMind;
 
 public partial class App : Application
 {
-    [STAThread]
-    public static void Main()
-    {
-        VelopackApp.Build().Run();
-        var app = new App();
-        app.InitializeComponent();
-        app.Run();
-    }
 }
