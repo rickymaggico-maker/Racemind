c1abe7a0f9bfd476b44a59ba4c0a62b040cd9b9c
rickymaggico-maker <ricky.maggico@gmail.com>
2026-09-09 13:13:51 +0200

    Add manual update check support

--- src/RaceMind/Services/UpdateService.cs
+++ src/RaceMind/Services/UpdateService.cs
@@ -3,15 +3,25 @@ using Velopack.Sources;
 
 namespace RaceMind.Services;
 
+public enum UpdateCheckResult
+{
+    UpToDate,
+    UpdateAvailable,
+    Error
+}
+
 public sealed class UpdateService
 {
     private const string RepoUrl = "https://github.com/rickymaggico-maker/Racemind";
+
+    private static UpdateManager CreateManager() =>
+        new(new GithubSource(RepoUrl, null, false));
 
     public async Task<bool> CheckAndApplyAsync()
     {
         try
         {
-            var mgr = new UpdateManager(new GithubSource(RepoUrl, null, false));
+            var mgr = CreateManager();
             var update = await mgr.CheckForUpdatesAsync();
             if (update is null) return false;
             await mgr.DownloadUpdatesAsync(update);
@@ -19,5 +29,27 @@ public sealed class UpdateService
             return true;
         }
         catch { return false; }
+    }
+
+    public async Task<UpdateCheckResult> CheckManualAsync(bool installWhenAvailable = true)
+    {
+        try
+        {
+            var mgr = CreateManager();
+            var update = await mgr.CheckForUpdatesAsync();
+            if (update is null) return UpdateCheckResult.UpToDate;
+
+            if (installWhenAvailable)
+            {
+                await mgr.DownloadUpdatesAsync(update);
+                mgr.ApplyUpdatesAndRestart(update);
+            }
+
+            return UpdateCheckResult.UpdateAvailable;
+        }
+        catch
+        {
+            return UpdateCheckResult.Error;
+        }
     }
 }
