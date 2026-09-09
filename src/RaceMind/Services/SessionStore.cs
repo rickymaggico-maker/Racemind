de34dc365c4d09f5893baa1b14b2856c261d1af0
rickymaggico-maker <ricky.maggico@gmail.com>
2026-09-09 17:12:29 +0200

    Persist deep telemetry as session backup

--- src/RaceMind/Services/SessionStore.cs
+++ src/RaceMind/Services/SessionStore.cs
@@ -19,13 +19,21 @@ public sealed class SessionStore
             var exists = File.Exists(file);
             await using var sw = new StreamWriter(file, append: true);
             if (!exists)
-                await sw.WriteLineAsync("timestamp,driver,driver_steam_id,vehicle,track,lap,speed_kph,throttle,brake,steering,gear,fuel_l,in_garage,in_pit_lane");
+                await sw.WriteLineAsync("timestamp,driver,driver_steam_id,vehicle,track,lap,speed_kph,throttle,brake,steering,gear,fuel_l,accel_x,accel_y,accel_z,pitch_rate,yaw_rate,roll_rate,front_ride_height_m,rear_ride_height_m,front_downforce_n,rear_downforce_n,rear_brake_bias,abs_active,tc_active,front_arb,rear_arb,fl_grip,fr_grip,rl_grip,rr_grip,fl_pressure_kpa,fr_pressure_kpa,rl_pressure_kpa,rr_pressure_kpa,fl_temp_c,fr_temp_c,rl_temp_c,rr_temp_c,fl_load_n,fr_load_n,rl_load_n,rr_load_n,fl_brake_temp_c,fr_brake_temp_c,rl_brake_temp_c,rr_brake_temp_c,in_garage,in_pit_lane");
+
             await sw.WriteLineAsync(string.Join(',',
                 s.Timestamp.ToString("O", CultureInfo.InvariantCulture), Csv(s.Driver), s.DriverSteamId.ToString(CultureInfo.InvariantCulture),
                 Csv(s.Vehicle), Csv(s.Track), s.Lap,
-                s.SpeedKph.ToString(CultureInfo.InvariantCulture), s.Throttle.ToString(CultureInfo.InvariantCulture),
-                s.Brake.ToString(CultureInfo.InvariantCulture), s.Steering.ToString(CultureInfo.InvariantCulture), s.Gear,
-                s.FuelLitres.ToString(CultureInfo.InvariantCulture), s.InGarage, s.InPitLane));
+                N(s.SpeedKph), N(s.Throttle), N(s.Brake), N(s.Steering), s.Gear, N(s.FuelLitres),
+                N(s.LocalAccelX), N(s.LocalAccelY), N(s.LocalAccelZ), N(s.PitchRateRadS), N(s.YawRateRadS), N(s.RollRateRadS),
+                N(s.FrontRideHeightM), N(s.RearRideHeightM), N(s.FrontDownforceN), N(s.RearDownforceN), N(s.RearBrakeBias),
+                s.AbsActive, s.TcActive, s.FrontAntiRollBar, s.RearAntiRollBar,
+                N(s.FrontLeft.GripFraction), N(s.FrontRight.GripFraction), N(s.RearLeft.GripFraction), N(s.RearRight.GripFraction),
+                N(s.FrontLeft.PressureKpa), N(s.FrontRight.PressureKpa), N(s.RearLeft.PressureKpa), N(s.RearRight.PressureKpa),
+                N(AvgTemp(s.FrontLeft)), N(AvgTemp(s.FrontRight)), N(AvgTemp(s.RearLeft)), N(AvgTemp(s.RearRight)),
+                N(s.FrontLeft.TireLoadN), N(s.FrontRight.TireLoadN), N(s.RearLeft.TireLoadN), N(s.RearRight.TireLoadN),
+                N(s.FrontLeft.BrakeTempC), N(s.FrontRight.BrakeTempC), N(s.RearLeft.BrakeTempC), N(s.RearRight.BrakeTempC),
+                s.InGarage, s.InPitLane));
         }
         finally
         {
@@ -33,5 +41,7 @@ public sealed class SessionStore
         }
     }
 
+    private static double AvgTemp(WheelTelemetry w) => (w.TempLeftC + w.TempCenterC + w.TempRightC) / 3.0;
+    private static string N(double value) => value.ToString(CultureInfo.InvariantCulture);
     private static string Csv(string value) => '"' + value.Replace("\"", "\"\"") + '"';
 }
